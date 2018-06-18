7e3967a9ed44dbf62fad1bd498e22ab1f1d8fca2
willytell <guille.torres@gmail.com>
2018-06-18 10:33:02 +0200

    modified batch file windows

--- run-win10.bat
+++ run-win10.bat
@@ -1,10 +1,10 @@
-#!/bin/bash
+:: batch file for windows
 
 echo "begining..."
 
 echo "====>"
 
-CUDA_VISIBLE_DEVICES=0 python3 main.py -c config/conf_dist1-resnet50-with_bbox-crop-da-francho.py -a train &>logs/dist1-resnet50-with_bbox-crop-da-francho-train
+python3 main.py -c config/conf_dist1-resnet50-with_bbox-crop-da-francho.py -a train &>logs/dist1-resnet50-with_bbox-crop-da-francho-train
 
 
 echo "finished!"
