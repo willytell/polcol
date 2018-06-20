f89165a105155dc1bbfd10084c9d33942b3eeac8
willytell <guille.torres@gmail.com>
2018-06-20 13:59:06 +0200

    adding config file

--- run-win10.bat
+++ run-win10.bat
@@ -4,7 +4,10 @@ echo "begining..."
 
 echo "===="
 
-python main.py -c config\conf_dist1-resnet50-with_bbox-crop-da-francho.py -a train > logs\dist1-resnet50-with_bbox-crop-da-francho-train 2>&1
+python main.py -c config\conf_dist1-resnet50-with_bbox-da-class_weigths-imagenet-francho.py -a train > logs\conf_dist1-resnet50-with_bbox-da-class_weigths-imagenet-francho-train 2>&1
+
+
+#python main.py -c config\conf_dist1-resnet50-with_bbox-crop-da-francho.py -a train > logs\dist1-resnet50-with_bbox-crop-da-francho-train 2>&1
 
 
 echo "finished!"
