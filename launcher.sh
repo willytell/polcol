aecb2546d633e9a47d8a15ff446577a8ae3a17dc
willytell <guille.torres@gmail.com>
2018-07-12 16:35:42 +0200

    launcher for the cluster

--- launcher.sh
+++ launcher.sh
@@ -1,14 +1,15 @@
 # Con este fichero lanzamos un trabajo que nos permite ver que valor contendrá en el entorno
 # CUDA_VISIBLE_DEVICES al solicitar la cantidad de gráficas que queramos.
 
-for e in `seq 0 9`; do
-   for k in `seq 0 4`; do
-      sbatch ./run-cluster.sh $e $k
-   done
-done
+#for e in `seq 0 9`; do
+#   for k in `seq 0 4`; do
+#      sbatch ./run-cluster.sh $e $k
+#   done
+#done
 
-#sbatch ./run-cluster.sh 0 1
-#sbatch ./run-cluster.sh 0 2
+#sbatch ./run-cluster.sh 0 4
+#sbatch ./run-cluster.sh 4 1
+
 #sbatch ./run-cluster.sh 0 3
 #sbatch ./run-cluster.sh 0 4
 #
