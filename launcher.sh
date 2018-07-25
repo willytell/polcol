5e41d2e0b64d69d86394f10bc08209d06ac41361
willytell <guille.torres@gmail.com>
2018-07-25 20:06:20 +0200

    updating config files

--- launcher.sh
+++ launcher.sh
@@ -1,35 +1,27 @@
 # Con este fichero lanzamos un trabajo que nos permite ver que valor contendrá en el entorno
 # CUDA_VISIBLE_DEVICES al solicitar la cantidad de gráficas que queramos.
 
-#for e in `seq 6 9`; do
-#for e in `seq 3 5`; do
-#for e in `seq 0 2 `; do
-#   for k in `seq 0 8`; do
-#      sbatch ./run-cluster.sh $e $k
-#   done
-#done
-
 for k in `seq 0 8 `; do
    for e in `seq 0 9`; do
       sbatch ./run-cluster.sh $e $k
    done
 done
 
-#sbatch ./run-cluster.sh 0 0
-#sbatch ./run-cluster.sh 1 0
-#sbatch ./run-cluster.sh 3 4
-#sbatch ./run-cluster.sh 7 4
+#sbatch ./run-cluster.sh 0 2
+#sbatch ./run-cluster.sh 2 3
+#sbatch ./run-cluster.sh 3 3
+#sbatch ./run-cluster.sh 8 0
 #sbatch ./run-cluster.sh 9 4
 #sbatch ./run-cluster.sh 9 7
 
 # test
-#sbatch ./run-cluster.sh 0 6
+#sbatch ./run-cluster.sh 0 5
 #sbatch ./run-cluster.sh 1 4
 #sbatch ./run-cluster.sh 2 4
-#sbatch ./run-cluster.sh 3 4
-#sbatch ./run-cluster.sh 4 6
+#sbatch ./run-cluster.sh 3 0
+#sbatch ./run-cluster.sh 4 0
 #sbatch ./run-cluster.sh 5 0
-#sbatch ./run-cluster.sh 6 0
-#sbatch ./run-cluster.sh 7 0
-#sbatch ./run-cluster.sh 8 4
-#sbatch ./run-cluster.sh 9 4
+#sbatch ./run-cluster.sh 6 5
+#sbatch ./run-cluster.sh 7 5
+#sbatch ./run-cluster.sh 8 6
+#sbatch ./run-cluster.sh 9 6
--- run-cluster.sh
+++ run-cluster.sh
@@ -5,7 +5,7 @@
 #SBATCH -t 5-00:05 # Runtime in D-HH:MM
 #SBATCH -p dcc # Partition to submit to
 #SBATCH --mem 16384 # 16GB solicitados.
-#SBATCH --gres gpu:Ti1080:1 # Para pedir Pascales MAX 8
+#SBATCH --gres gpu:Pascal:1 # Para pedir Pascales MAX 8
 #SBATCH -o %x_%u_%j.out # File to which STDOUT will be written
 #SBATCH -e %x_%u_%j.err # File to which STDERR will be written
 
@@ -14,14 +14,14 @@ kfold=$2
 
 echo "begining..."
 
-echo "Experiment: Results6-partH02"
+echo "Experiment: Results6-partE02_dist51_resnet50"
 echo "Dataset: images from BBox"
-echo "Configuration: without ImageNet and without data augmentation"
+echo "Configuration: with ImageNet and without data augmentation"
 
-echo "dist50, experiment $exp, kfold $kfold"
+echo "dist51, experiment $exp, kfold $kfold"
 
-python3 main.py -c config/conf_dist50-resnet50-bbox-imagenet-exp"$exp".py -a train -e $exp -k $kfold &>logs/dist50-resnet50-bbox-imagenet-exp"$exp"-kfold"$kfold"-train
-#python3 main.py -c config/conf_dist50-resnet50-bbox-imagenet-exp"$exp".py -a test  -e $exp -k $kfold &>logs/dist50-resnet50-bbox-imagenet-exp"$exp"-test
+python3 main.py -c config/conf_dist51-resnet50-bbox-imagenet-exp"$exp".py -a train -e $exp -k $kfold &>logs/dist51-resnet50-bbox-imagenet-exp"$exp"-kfold"$kfold"-train
+#python3 main.py -c config/conf_dist51-resnet50-bbox-imagenet-exp"$exp".py -a test  -e $exp -k $kfold &>logs/dist51-resnet50-bbox-imagenet-exp"$exp"-test
 
 #echo "dist3"
 #python3 main.py -c config/conf_dist3-resnet50-with_bbox-without-imagenet-with-da-exp"$exp".py -a train -e $exp -k $kfold &>logs/dist3-resnet50-with_bbox-without-imagenet-with-da-exp"$exp"-kfold"$kfold"-train
